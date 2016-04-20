00f8a84e46b9d7ca239479021f108a5646ef1478
Sally Pang <sally.pang@dynamic-leap.com>
2016-04-20 13:19:29 -0700

    Initial Commit

---
+++ A Waste of Money/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A Waste of Money
+//
+//  Created by Sally Pang on 2016-04-20.
+//  Copyright © 2016 Sally Pang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ A Waste of Money/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  A Waste of Money
+//
+//  Created by Sally Pang on 2016-04-20.
+//  Copyright © 2016 Sally Pang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
